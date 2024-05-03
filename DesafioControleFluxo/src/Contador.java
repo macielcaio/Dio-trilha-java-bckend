1dc2ea1e1af51d22e884e86ef9afd69d7aa1aee2
macielcaio <caiohenriquemaciel@gmail.com>
2024-05-03 14:19:26 -0300

    Desafio contador com exception customizada

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			//imprime mensagem de exception
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+            int contagem = parametroDois - parametroUm;
+            if (parametroDois < parametroUm) {
+                throw new ParametrosInvalidosException();
+            }else{
+                for (int i = 0; i < contagem; i++) {
+                    System.out.println("Imprindo o valor " + (i+1));
+                }
+            }
+        }
+}
